453937586e1cf0c8f661a70029c177cd43555821
Kevin Scannell <kscanne@gmail.com>
2004-03-15 20:07:39 +0000

    Various changes trying to debug mysterious DB->close crash. Ended up being that I was messing with tail of returned data (to add a null termination) that I shouldn't have been. All other changes cosmetic.

--- get.c
+++ get.c
@@ -4,23 +4,13 @@
 #include <string.h>
 #include <db.h>
 
-void
-cleanup (DB * dbp, int ret)
-{
-  int t_ret;
-
-  if ((t_ret = dbp->close (dbp, 0)) != 0 && ret == 0)
-    ret = t_ret;
-  exit (ret);
-}
-
 int
 main (int argc, char* argv[])
 {
   DB *dbp;
   DBT key, data;
-  int ret,len;
-  char token[65535];
+  int ret,len,t_ret;
+  char token[1024];
 
   if (argc != 2)
   {
@@ -37,32 +27,31 @@ main (int argc, char* argv[])
        dbp->open (dbp, NULL, argv[1], NULL, DB_HASH, DB_RDONLY, 0)) != 0)
     {
       dbp->err (dbp, ret, "%s", argv[1]);
-      cleanup (dbp, ret);
+      goto err;
     }
 
-  while (fgets (token, 65535, stdin) != NULL)
+  while (fgets (token, 1024, stdin) != NULL)
     {
       memset (&key, 0, sizeof (key));
       memset (&data, 0, sizeof (data));
       len=strlen(token);
       token[len-1]=0;
-      key.size = len-1;
-      key.data = malloc (key.size + 1);
-      strcpy (key.data, token);
+      key.size = len;  // includes null
+      key.data = token;
       if ((ret = dbp->get (dbp, NULL, &key, &data, 0)) == 0) {
-         ((char*) data.data)[data.size]=0;
 	 printf ("%s\n", (char *) data.data);
-	  ;
 	}
       else
 	{
-	  fprintf(stderr, "Could not find key %s\n", key.data);
-	  dbp->err (dbp, ret, "DB->get");
-	  cleanup (dbp, ret);
+//	 fprintf(stderr, "Could not find key %s\n", key.data);
+//	 dbp->err (dbp, ret, "DB->get");
+//	 goto err;
+         ;
 	}
-      free (key.data);
- //     free (data.data);
     }
 
-  cleanup (dbp, ret);
+  err: 
+   if ((t_ret = dbp->close (dbp, 0)) != 0 && ret == 0)
+    ret = t_ret;
+  exit (ret);
 }
