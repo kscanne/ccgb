99ca90328076e84ec9bb12559dfb9c18c95e1e89
Kevin Scannell <kscanne@gmail.com>
2004-09-28 03:20:58 +0000

    New file abstracting out the problematic shell code for grepping quoted search terms and maximum hit count.   The point is that in a pipes and filters architecture,   "head -n MAX" doesn't cut it, since it stops reading   input from the stream causing "broken pipe" errors;   we want a way of stopping processing entirely when   we get to max count.

---
+++ quotekeep.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+open (INFILE, "<:bytes", $ARGV[0]) or die "Can't open file: $!";
+my $max = $ARGV[1];
+my $oksofar = 0;
+shift @ARGV;
+shift @ARGV;
+my @regexps;
+foreach (@ARGV) {
+	push @regexps, qr/$_/ if (m/ /);
+}
+while (<INFILE>) {
+	my $ok = 1;
+	foreach my $patt (@regexps) {
+		unless (m/$patt/) {
+			$ok = 0;
+			last;
+		}
+	}
+	if ($ok) {
+		s/ .*//;
+		print;
+		$oksofar++;
+		last if ($oksofar == $max);
+	}
+}
+close INFILE;
+exit 0;
