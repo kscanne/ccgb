57c5c1b48b81323c1197fa54d17143d7c35abfd8
Kevin Scannell <kscanne@gmail.com>
2004-12-07 02:02:43 +0000

    got rid of silly interpolation

--- cc.pl
+++ cc.pl
@@ -3,7 +3,7 @@ use Frontier::Client;
 use Encode 'from_to';
 $server_url = 'http://borel.slu.edu:8080/RPC2';
 $server = Frontier::Client->new(url => $server_url,  debug => 0, );
-my @textinput = <>;
-my $toserver = "@textinput";
+local $/; # slurp
+my $toserver = <STDIN>;
 from_to($toserver, "iso-8859-1", "utf-8");
 print $server->call('gaeilge.corpas', $toserver);
